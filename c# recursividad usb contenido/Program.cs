7c908453216231e495103a154ce1e393609cae8f
Santellana179 <imjuan179@gmail.com>
2023-08-30 21:12:12 -0500

    Agregar archivos de proyecto.

---
+++ c# recursividad usb contenido/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Ingresa ruta de almacenamiento: ");
+string usb = Console.ReadLine();
+
+if (Directory.Exists(usb))
+{
+    Console.WriteLine("Contenido del almacenamiento:");
+    try
+    {
+        ContenidoRuta(usb);
+    }
+    catch (UnauthorizedAccessException)
+    {
+        Console.WriteLine("No tienes acceso a uno o más elementos en la memoria USB.");
+    }
+}
+else
+{
+    Console.WriteLine("La ruta no es válida.");
+}
+static void ContenidoRuta(string direcion)
+{
+    Console.WriteLine($"Contenido de {direcion}:");
+
+    try
+    {
+        string[] directories = Directory.GetDirectories(direcion);
+        foreach (string directory in directories)
+        {
+            Console.WriteLine($"[Directorio] {Path.GetFileName(directory)}");
+            ContenidoRuta(directory);
+        }
+        string[] archivos = Directory.GetFiles(direcion);
+        foreach (string file in archivos)
+        {
+            Console.WriteLine($"[Archivo] {Path.GetFileName(file)}");
+        }
+    }
+    catch (UnauthorizedAccessException)
+    {
+        Console.WriteLine($"No tienes acceso a {direcion}.");
+    }
+}
